6512ae8700869370f4a2c6a5a370f7a312d6bef1
Vincenzo Iacona <vincenzoiacona@hotmail.com>
2022-04-05 13:06:05 +0200

    Header and Brand section done

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import './header.css'
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='gpt3__header section__padding' id='home'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>
+          Let's Build Something Amazing with Code Academy
+        </h1>
+        <p>Technical Challenge: To be done in 72h starting from the time of this email, this will show us your technical skills in action and how well you can adapt to challenges and strict deadlines.</p>
+        <div className='gpt3__header-content-input'>
+          <input type="email" placeholder='Your Email Address'/>
+          <button type='button'>Get Started</button>
+        </div>
+        <div className='gpt3__header-content-people'>
+          <img src={people} alt="people"/>
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+      </div>
+        <div className='gpt3__header-image'>
+            <img src={ai} alt="Ai"/>
+        </div>
+      
+    </div>
   )
 }
 
